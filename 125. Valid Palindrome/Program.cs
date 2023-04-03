042faee79d3d58fcf32f12dfe9d3fe0655b5c551
eimdadul_haque <eimdadulhaque@gmail.com>
2023-04-03 21:17:57 +0600

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var res = IsPalindrome("race a car");
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        int left = 0,
+            right = s.Length- 1;
+
+        while(left < right) 
+        {
+            while(left < right && !IsAlphanumeric(s[left]))
+                left++;
+            while (right > left && !IsAlphanumeric(s[right]))
+                right--;
+            if (s[right].ToString().ToLower() 
+                != s[right].ToString().ToLower())
+                return false;
+            left++; right--;
+        }
+
+        return true;
+    }
+
+    public static bool IsAlphanumeric(char ch)
+    {
+        if('A' <= ch && ch <= 'Z' 
+            || 'a' <= ch && ch <= 'z'
+            || '0' <= ch && ch <= '9' )
+            return true;
+        return false;
+    }
+}
