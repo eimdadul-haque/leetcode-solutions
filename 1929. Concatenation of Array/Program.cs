ab8efeab03382df0c7cbc1801e98bec98574ee99
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-04-20 16:31:56 +0600

    1929. Concatenation of Array

---
+++ 1929. Concatenation of Array/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program { 
+    public static void Main(string[] args)
+    {
+        var nums = new int[] { 3, 4, 5 };
+        var solution = new Solution();
+        var result = solution.GetConcatenation(nums);
+    }
+}
+
+public class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int index = 0;
+        var outputArray = new int[nums.Length * 2];
+        for (int i = 1; i <= 2; i++)
+            foreach (var num in nums)
+                outputArray[index++] = num;
+
+        return outputArray;
+    }
+}
+
