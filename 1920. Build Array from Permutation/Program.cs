d9a6cfa28b72ce1dfb0e26a465789447f8ac00b7
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-05-06 23:50:48 +0600

    1920 Build Array from Permutation

---
+++ 1920. Build Array from Permutation/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program {
+    public static void Main(string[] args) {
+        var nums = new int[] { 0, 2, 1, 5, 3, 4 };
+        var solution = new Solution();
+        var result = solution.BuildArray(nums); ;
+    }
+}
+
+public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        for(int i = 0; i < nums.Length; i++)
+            result[i] = nums[nums[i]];
+        return nums;
+    }
+}
