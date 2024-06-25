46f08182ccb1999bbd3b892dcb616222469dff08
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-06-25 22:10:29 +0600

    905. Sort Array By Parity

---
+++ 905. Sort Array By Parity/Program.cs
@@ -0,0 +1,34 @@
+﻿
+var nums = new int[] { 1, 3, 5, 7, 2, 4, 6, 8 };
+var solution = new Solution();
+var result = solution.SortArrayByParity(nums);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);   
+}
+
+
+public class Solution
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        int last = nums.Length - 1, first = 0;
+        var newNums = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                newNums[first] = nums[i];
+                first++;
+            }
+            else
+            {
+                newNums[last] = nums[i];
+                last--;
+            }
+        }
+
+        return newNums;
+    }
+}
