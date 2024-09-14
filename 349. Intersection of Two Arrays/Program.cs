1382872151fb303a034a0f9e3d4f77d8c5f88228
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-09-14 12:39:29 +0600

    349. Intersection of Two Arrays

---
+++ 349. Intersection of Two Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿var nums1 = new int[] { 1, 2, 2, 1 };
+var nums2 = new int[] { 2, 2 };
+
+var solution = new Solution();
+var result = solution.Intersection(nums1, nums2);
+
+
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var temp = new List<int>();
+        foreach (var num1 in nums1)
+        {
+            foreach (var num2 in nums2)
+            {
+                if(num1 == num2)
+                {
+                    temp.Add(num1);   
+                }
+            }
+        }
+
+        var result = new List<int>();
+        foreach (var item in temp)
+        {
+            if (!result.Contains(item))
+            {
+                result.Add(item);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
