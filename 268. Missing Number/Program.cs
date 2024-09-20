53cc8fe84c85844601458f8a80348cf4a1ccb849
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-09-20 13:19:10 +0600

    268. Missing Number

---
+++ 268. Missing Number/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var nums = new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+var solution = new Solution();
+int result = solution.MissingNumber(nums);
+int a = result;
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        int gSum = n * (n + 1) / 2;
+        int sum = 0;
+
+        foreach (var num in nums)
+        {
+            sum = sum + num;
+        }
+
+        return gSum - sum;
+    }
+}
