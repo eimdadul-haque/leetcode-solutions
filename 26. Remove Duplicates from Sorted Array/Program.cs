cfbc31b3c91bdac80e43fff8bb8a5e80859389bf
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-10-05 18:30:50 +0600

    26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,22 @@
+﻿var nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+var solution = new Solution();
+var result = solution.RemoveDuplicates(nums);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+        int first = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[first])
+            {
+                first++;
+                nums[first] = nums[i];
+            }
+        }
+        return first + 1;
+    }
+}
